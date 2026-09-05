3e5605e29feb8a3b0e35fd82316ede478fb0adf8
Eliott Wattelier <eliott.wattelier@nyll.tech>
2026-09-05 16:24:04 +0200

    Increased barracks weight

--- src/main/java/kniumm/mercenaries/ModStructurePoolElements.java
+++ src/main/java/kniumm/mercenaries/ModStructurePoolElements.java
@@ -15,14 +15,14 @@ public class ModStructurePoolElements {
         Registry<StructureTemplatePool> templatePoolRegistry = server.registryAccess().lookupOrThrow(Registries.TEMPLATE_POOL);
 
         Identifier plainsPoolLocation = Identifier.parse("minecraft:village/plains/houses");
-        Identifier savannaPoolLocation = Identifier.parse("minecraft:village/savanna/houses");
-        Identifier taigaPoolLocation = Identifier.parse("minecraft:village/taiga/houses");
+        // Identifier savannaPoolLocation = Identifier.parse("minecraft:village/savanna/houses");
+        // Identifier taigaPoolLocation = Identifier.parse("minecraft:village/taiga/houses");
 
         Identifier plainsBarracks = Identifier.fromNamespaceAndPath(Mercenaries.MOD_ID, "village/plains/houses/barracks");
-        Identifier savannaBarracks = Identifier.fromNamespaceAndPath(Mercenaries.MOD_ID, "village/savanna/houses/barracks");
-        Identifier taigaBarracks = Identifier.fromNamespaceAndPath(Mercenaries.MOD_ID, "village/taiga/houses/barracks");
+        // Identifier savannaBarracks = Identifier.fromNamespaceAndPath(Mercenaries.MOD_ID, "village/savanna/houses/barracks");
+        // Identifier taigaBarracks = Identifier.fromNamespaceAndPath(Mercenaries.MOD_ID, "village/taiga/houses/barracks");
 
-        StructurePoolHelper.addLegacyElementToPool(templatePoolRegistry, plainsPoolLocation, plainsBarracks, 30);
+        StructurePoolHelper.addLegacyElementToPool(templatePoolRegistry, plainsPoolLocation, plainsBarracks, 80);
         // StructurePoolHelper.addLegacyElementToPool(templatePoolRegistry, savannaPoolLocation, savannaBarracks, 5);
         // StructurePoolHelper.addLegacyElementToPool(templatePoolRegistry, taigaPoolLocation, taigaBarracks, 5);
     }
